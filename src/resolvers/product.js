cd7e59784f8e026a0005338c4dcf98d34142f0ea
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-12-09 20:14:05 -0800

    remove hello console.log

--- src/resolvers/product.js
+++ src/resolvers/product.js
@@ -15,7 +15,6 @@ export default {
       return await Product.findByPk(id)
     },
     getAllProducts: async (parent, args, { Product }) => {
-      console.log('hello')
       return await Product.findAll()
     },
   },
